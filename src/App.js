af039c138374bee5c6e0a72647723c7f1a4f0132
Matias Agobian <comparo.agobian@gmail.com>
2021-05-07 03:01:38 -0300

    fix stuff

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
-import HomePageContainer from "./Containers/homePage/HomePageContainer";
-import ItemsListContainer from "./Containers/itemsListPage/ItemsListContainer";
-import ItemDetailContainer from "./Containers/ItemDetailPage/ItemDetailContainer";
+import HomePageContainer from "./container/homePage/HomePageContainer";
+import ItemsListContainer from "./container/ItemDetailPage/ItemDetailContainer";
+import ItemDetailContainer from "./container/ItemDetailPage/ItemDetailContainer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { CartContext } from "./context/CartContext";
 
